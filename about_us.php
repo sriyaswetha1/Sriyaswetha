8da6eb2596223fa40be412b26d5d95f55db74f26
sriyaswetha1 <49361937+sriyaswetha1@users.noreply.github.com>
2019-08-27 20:45:27 +0530

    Add files via upload

---
+++ about_us.php
@@ -0,0 +1,77 @@
+<?php
+require "includes/common.php";
+?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>About Us | E-Store</title>
+        <link rel="shortcut icon" href="img\srtcticon.png" type="image/png">
+        
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="css/style.css" rel="stylesheet">
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+    
+      
+<body>
+       <!-- Header -->
+    <?php include 'includes/header.php'; ?>
+    <br>
+        <div class="container">
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:grey" class="color">WHO WE ARE</h3>
+                    <img src="./img/about-img.jpg" alt="about image"><br><br>
+                    <p>E-store is an American electronic connerce company with headquarters in Washington.It is the largest Internet-basedretailer in the United States.E-Store started as an online blog,but soon diversified,selling mobile phones.E-Store also sells certain low-end products like USB cables and other accessories.E-Store has separate retail websites for United States,United Kingdom & Ireland,France, Canada, Germany,Italy, Spain, the Netherlands, Australia, Brazil, Japan, China, India and Mexico.Amazon alsooffers international shipping to certain other users. <p>
+                </div>
+                </div>
+            </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:grey" class="color">OUR HISTORY</h3><br><br>
+                    <h4 style="color:blue">1988&#45;</h4>
+                    <p>The company was founded in 1998,spurred by what Velos called his "Initiating framework",which described his as an initiate to participate in the Internet business boom during that time.In1998,Velos left his employment as president of Ofcol & Co. and moves to seattle.He began to work on a business plan for what would eventuslly become E-Stre.</p>
+                    <br>
+                    <h4 style="color:blue">2002&#45;</h4>
+                    <p>In January 2002, E-Store has received a funding of $12 million from Venture Partners and Indo-US Venture Partners.</p>
+                    <br>
+                    <h4 style="color:blue">2008&#45;</h4>
+                    <p>In July 2008,the company raised further $45 million from Bessemer Venture Parthers,along with existing investors Venture Partners and Indo-US Venture Partners.</p>
+					<br>
+					<h4 style="color:blue">2015&#45;</h4>
+					<p>E- Store received its 3rd round of funding of $133 million on Feb-2015.The 3rd round of funding was led by Fcom with all the current institutional investors, including Kalaari Capital, Venture partners.</p>
+                    <br>
+					</div>
+                </div>
+                </div>
+            
+            
+            <div class="col-sm-4">
+                <div class="panel panel-default">
+                   <div class="panel-body">
+                    <h3 style="color:grey" class="color">OPPORTUNITIES</h3><br><br>
+                        <b>Available Roles</b>
+                            <ol>
+                                <li>Jr./Sr. Web Developer [Full Time Role + also available as a 6 Months Internships]</li>
+                                <li>Bussiness Apprentice [6 Months Internships]</li>
+                                <li>Manager at backend operations[Full Time Role + also available as a 6 Months Internships]</li>
+                            </ol>
+                        </div>
+                </div>
+            </div>
+            
+        </div>
+        <?php include 'includes/footer.php'; ?>
+    </body>
+</html>
